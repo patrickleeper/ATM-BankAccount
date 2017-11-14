ec62a4ad17f59a37edd03db96753471206d6776f
patrickleeper <32469087+patrickleeper@users.noreply.github.com>
2017-11-14 11:41:29 -0500

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -3,70 +3,60 @@ import javax.swing.*;
     public class BankAccount 
    {
    
-      private int[] accountNumber;
-      private int[] accountPIN;
-      private double[] accountBalance;
+      private int accountNumber;
+      private int accountPIN;
+      private double accountBalance;
       
       public BankAccount()
       {
-      
-      }
-      
-      public BankAccount(int[] accountNumber, int[] accountPIN, double[] accountBalance)
-      {
-         setAccountNumber(accountNumber);
-         setAccountPIN(accountPIN);
-         setAccountBalance(accountBalance);
+         int number = accountNumber;
+         int pin = accountPIN;
+         double balance = accountBalance;
          
          return;
       }
       
-      public void setAccountNumber(int[] accountNumber)
+      public BankAccount(int number, int pin, double balance)
       {
-         accountNumber = new int[5];
-         accountNumber[0] = 1000;
-         accountNumber[1] = 2000;
-         accountNumber[2] = 3000;
-         accountNumber[3] = 4000;
-         accountNumber[4] = 5000;
+         setAccountNumber(number);
+         setAccountPIN(pin);
+         setAccountBalance(balance);
+         
          return;
       }
       
-      public void setAccountPIN(int[] accountPIN)
+      public void setAccountNumber(int number)
       {
-         accountPIN = new int[5];
-         accountPIN[0] = 9999;
-         accountPIN[1] = 9998;
-         accountPIN[2] = 9997;
-         accountPIN[3] = 9996;
-         accountPIN[4] = 9995;
+         
          return;
       }
       
-      public void setAccountBalance(double[] accountBalance)
+      public void setAccountPIN(int pin)
       {
-         accountBalance = new double[5];
-         accountBalance[0] = 500.75;
-         accountBalance[1] = 1001.09;
-         accountBalance[2] = 1501.43;
-         accountBalance[3] = 2001.77;
-         accountBalance[4] = 2502.11;
+         
          return;
       }
       
-      public int[] getAccountNumber()
+      public void setAccountBalance(double balance)
       {
-         return accountNumber;
+         
+         return;
       }
       
-      public double[] getAccountBalance()
+      public int getAccountNumber()
       {
-         return accountBalance;
+         return number;
       }
       
-      public void verifyPIN(int[] accountPIN)
+      public double getAccountBalance()
       {
-         boolean verifyPIN;
+         return balance;
+      }
+      
+      public boolean verifyPIN(int pin)
+      {
+         
+         return pin;   
       }
       
       public void withdrawFunds()
